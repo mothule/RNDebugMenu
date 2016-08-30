b492064d19a8497068df144ce92a84dc9bdbf84f
Motoki Kawakami <mothule.dev@gmail.com>
2016-08-30 23:29:42 +0900

    Remake Project for Cocoa Framework Library.

---
+++ RNDebugMenu/RNDebugMenu.h
@@ -0,0 +1,19 @@
+//
+//  RNDebugMenu.h
+//  RNDebugMenu
+//
+//  Created by mothule on 2016/08/30.
+//  Copyright © 2016年 mothule. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RNDebugMenu.
+FOUNDATION_EXPORT double RNDebugMenuVersionNumber;
+
+//! Project version string for RNDebugMenu.
+FOUNDATION_EXPORT const unsigned char RNDebugMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNDebugMenu/PublicHeader.h>
+
+
